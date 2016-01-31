171a075122f570095982175ee95ca07205d559fe
Askhat Sakhabiev <jojbot@gmail.com>
2016-01-31 13:43:03 +0300

    add out funk

---
+++ out.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void op(vector<vector<int> > a)
+{
+    cout << "\033[2J\033[1;1H";
+    for (int i = 0; i < (int)a.size(); i++)
+    {
+        for (int j = 0; j < (int)a[i].size(); j++)
+        {
+            cout << a[i][j] << " ";
+        }
+    }
+    cout << endl;
+}
+
