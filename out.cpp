31e316c34026b24b91c4abb4111a254d6693a17e
Askhat Sakhabiev <jojbot@gmail.com>
2016-01-31 13:58:55 +0300

    link

--- out.cpp
+++ out.cpp
@@ -10,8 +10,9 @@ void op(vector<vector<int> > a)
     {
         for (int j = 0; j < (int)a[i].size(); j++)
         {
-            cout << a[i][j] << " ";
+            cout << a[i][j];
         }
+        cout << endl;
     }
     cout << endl;
 }
